c7516d0fb6546faff886d2fc00c2b3a14bb69dd4
dinesh <140582887+Dinesh-raya@users.noreply.github.com>
2023-09-28 19:05:51 +0530

    Update main.py backround

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ import time
 
 
 st.set_page_config(page_title='Streamlit', page_icon='🐼', initial_sidebar_state='collapsed')
-st.subheader(':rainbow[HI THERE WELCOME TO :_YOUTUBE VIDEO DOWNLOADER_] :sunglasses:')
 
 st.markdown(f"""
             <style>
-            .stApp {{background-image: url("https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1942&q=80");
+            .stApp {{background-image: url("https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80");
                      background-attachment: fixed;
                      background-size: cover}}
          </style>
